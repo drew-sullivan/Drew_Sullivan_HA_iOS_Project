75fb77c6424564c0a66678baedbe377ab2c8c9cc
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-12-27 09:48:51 -0500

    Removing default vc and adding ProsViewController

---
+++ New Group/ProsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProsViewController.swift
+//  Drew_Sullivan_HA_iOS_Project
+//
+//  Created by Drew Sullivan on 12/27/18.
+//  Copyright © 2018 Drew Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ProsViewController: UITableViewController {
+    
+}
